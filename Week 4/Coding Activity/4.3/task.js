0ab656038955db4a7b31fd172f16a247feeb82b6
N.MATHANKUMAR <92289475+mathankumar28@users.noreply.github.com>
2021-10-31 09:22:20 -0700

    Update task.js

--- Week 4/Coding Activity/4.3/task.js
+++ Week 4/Coding Activity/4.3/task.js
@@ -1,12 +1,15 @@
 //Array flattenr activity
 // Define a function arrayFlattener with a 2 dimensional array as parameter:
 // example of 2 dimensional array: [1,[2,3],4,5]
-
+function arrayFlattener(){
+  var arr=[1,[2,3],4,5];
+  return arr;
+}
 //Return a new 1 dimensional array like, [1,2,3,4]
 
 //your code here
 //uncomment next line one by one, then check the console for results
- console.log(arrayFlattener([1,[2,3],4,5]))
+console.log(arr.flat())
 
 //don't change this line
 if (typeof module !== "undefined") {
